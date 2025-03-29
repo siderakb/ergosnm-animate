e4e0b87cecd534cbac876a1774a09103ae87af0c
ZiTe <honmonoh@gmail.com>
2025-03-29 11:27:00 +0800

    feat: add label for keycap

--- src/components/keycap.tsx
+++ src/components/keycap.tsx
@@ -1,22 +1,39 @@
-import { Rect, NodeProps } from "@motion-canvas/2d";
+import { Rect, NodeProps, Txt } from "@motion-canvas/2d";
 import {
   PossibleColor,
   PossibleVector2,
   SignalValue,
 } from "@motion-canvas/core";
 
+const LENGTH = 100;
+const RADIUS = 10;
+const FONT_SIZE = 36;
+const FONT_COLOR = "white";
+
 export interface KeycapProps extends NodeProps {
   position?: SignalValue<PossibleVector2>;
   fill?: SignalValue<PossibleColor>;
+  label?: string;
 }
 
 export class Keycap extends Rect {
   constructor(props?: KeycapProps) {
     super({
-      width: 100,
-      height: 100,
-      radius: 10,
+      width: LENGTH,
+      height: LENGTH,
+      radius: RADIUS,
       ...props,
     });
+
+    if (props?.label) {
+      this.add(
+        <Txt
+          text={props.label}
+          fontSize={FONT_SIZE}
+          fill={FONT_COLOR}
+          position={[0, 0]} // Center
+        />,
+      );
+    }
   }
 }
