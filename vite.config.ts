dcd379a51a1b26e196a568d10f763f92693beecd
ZiTe <honmonoh@gmail.com>
2025-03-29 09:32:27 +0800

    chore: add import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import motionCanvas from "@motion-canvas/vite-plugin";
 import ffmpeg from "@motion-canvas/ffmpeg";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [motionCanvas(), ffmpeg()],
 });
